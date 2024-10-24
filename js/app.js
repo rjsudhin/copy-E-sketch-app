308f6635afb590e54456554b91b9e9f38ecd1857
SudhinRaj <58643406+rjsudhin@users.noreply.github.com>
2024-10-24 22:13:17 +0530

    default drawing is working

--- js/app.js
+++ js/app.js
@@ -1,9 +1,24 @@
+loadingGrid(8)
+
 // loading drawing grid with size
 function loadingGrid(size) {
    const drawingGrid = document.querySelector('#drawing-grid')
    drawingGrid.style .gridTemplateColumns = `repeat(${size}, 1fr)`
    drawingGrid.style .gridTemplateRows = `repeat(${size}, 1fr)`
+
+   // creating of box in given size
+   const amountOfSize = size * size
+   for (let i = 0; i < amountOfSize; i += 1) {
+      let div = document.createElement('div')
+      div.classList.add('box')
+      // drawing event
+      div.addEventListener('mousemove', drawingGridBox)
+      drawingGrid.appendChild(div)
+   }
 }
 
+// drawing in the grid when the event occurs
+function drawingGridBox(e) {
+   e.target.style.backgroundColor = '#252525'
+}
 
-loadingGrid(8)
